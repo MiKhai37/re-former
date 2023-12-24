227812452d98fbfe873126c3b588f3143cb0e04f
MiKhai37 <mi.khai.thai@proton.me>
2023-12-24 18:08:17 +0100

    add README.md, create User model, migrate it, add vaildations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 24 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
