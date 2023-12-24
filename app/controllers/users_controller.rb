20cc24b44fa1ad337e6086143c18a806948fd055
MiKhai37 <mi.khai.thai@proton.me>
2023-12-24 21:09:49 +0100

    add edit routes and views, and add errors messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,24 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to edit_user_path(@user)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   # root "users#index"
 
-  resources :users, only: %i[new create edit]
+  resources :users, only: %i[new create edit update]
 end
